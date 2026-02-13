528e0e9c57ef22e7f75d86b984a97d3e4bac65ea
Aryan <aryanag382@gmail.com>
2026-02-13 20:35:10 +0530

    List(collection)

---
+++ JAVA_PRACTICE/src/UserList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserList {
+    public static void main(String[] args) {
+        List<Object> users=new ArrayList<>();
+        users.add("aryan");
+        users.add("summit");
+        users.add("rahul");
+        users.add(1);
+        for(Object user:users){
+            System.out.println(user);
+        }
+
+
+    }
+}
